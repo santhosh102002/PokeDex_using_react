fc5ffbe067fb297ed7f8d28b0962b64397b2e0de
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-09-13 19:41:53 +0530

    added react-dom-router

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -4,7 +4,7 @@ function Pokemon({name,url,id})
 {
     return(
 
-        <Link to={`/pokemon/:${id}`} className="pokemon-wrapper">
+        <Link to={`/pokemon/${id}`} className="pokemon-wrapper">
             <div className="pokemon">
                 <div className="pokemon-name" >{name}</div> 
                 <img className="pokemon-image" src={url} /> 
